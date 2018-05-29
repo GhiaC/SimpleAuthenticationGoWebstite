25c06fd0924e2fa29e149f5df36fad02e4bc83fb
ghiac <ghiasy@aut.ac.ir>
2018-05-29 15:01:02 +0430

    debuged in register

--- Controler/Register.go
+++ Controler/Register.go
@@ -23,15 +23,27 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 		if submit != "" && (username == "" || password == "") {
 			vars.Answer = "username or password is empty"
+		} else if hasUser(username) {
+			vars.Answer = "username has already been taken"
 		} else if username != "" && password != "" {
-
+			engine := GetEngine()
 			newUser := Models.NewUser(username, password)
-			affected, err := GetEngine().Insert(newUser)
-
+			affected, err := engine.Table("users").Insert(newUser)
+			println(affected)
 			if affected > 0 && err == nil {
-				vars.Answer = "you are Registered. Go to Login Page"
+				vars.Answer = "Successful. Go to Login Page"
 			}
 		}
-		OpenTemplate(w, r, vars, "login.html",Models.HeaderVariables{Title:"Register"})
+		OpenTemplate(w, r, vars, "login.html", Models.HeaderVariables{Title: "Register"})
 	}
+}
+
+func hasUser(username string) bool {
+	var id int
+	engine := GetEngine()
+	has, err := engine.Table("users").Where("username = ?", username).Cols("id").Get(&id)
+	if has && err == nil && id > 0 {
+		return true
+	}
+	return false
 }
